a3bcc2d0d02be60241514219e1403361454b23e4
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-23 15:46:43 +0100

    add update

--- backend-read_products/pages/index.js
+++ backend-read_products/pages/index.js
@@ -1,6 +1,7 @@
 import ProductForm from "@/components/ProductForm";
 import ProductList from "../components/ProductList";
 import styled from "styled-components";
+import useSWR from "swr";
 
 const Heading = styled.h1`
   text-align: center;
@@ -8,6 +9,27 @@ const Heading = styled.h1`
 `;
 
 export default function HomePage() {
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    console.log(productData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
   return (
     <>
       <Heading>
@@ -16,7 +38,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm />
+      <ProductForm onSubmit={handleAddProduct} value="" />
       <ProductList />
     </>
   );
